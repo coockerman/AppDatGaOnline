73d8f0eaff2b5e9df9df28b4460571c713c343c1
Nguyễn Hải <haibetmen997@gmail.com>
2024-12-13 14:55:01 +0700

    add food and combo

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -108,8 +108,234 @@ class _HomeViewState extends State<HomeView> {
           ),
         ),
       ),
-      body: Center(
-        child: Text('home'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Color(0xff32343E),
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Poppins',
+                  ),
+                  children: [
+                    TextSpan(
+                      text:
+                          'Hey ${controller.userData['fullName']?.toString() ?? 'User'} ,',
+                    ),
+                    const TextSpan(
+                      text: ' Have a good Day!',
+                      style: TextStyle(
+                        color: Color(0xffF44336),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity, // Chiều rộng cụ thể
+                height: 52, // Chiều cao cụ thể
+                child: TextField(
+                  controller: controller.searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search dishes, restaurants',
+                    hintStyle: const TextStyle(
+                        color: Color(0xff32343E),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14),
+                    prefixIcon: SizedBox(
+                      height: 16, // Chiều cao mới
+                      width: 16, // Chiều rộng mới
+                      child: Image.asset(
+                        ImageAsset.searchHome,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Image.asset(
+                        controller.isListening
+                            ? ImageAsset
+                                .microphone // Hiển thị hình ảnh micro khi đang nghe
+                            : ImageAsset
+                                .microphoneUn, // Hiển thị hình ảnh micro khi không nghe
+                        height:
+                            24, // Đặt chiều cao của hình ảnh (tuỳ chỉnh theo nhu cầu)
+                        width:
+                            24, // Đặt chiều rộng của hình ảnh (tuỳ chỉnh theo nhu cầu)
+                      ),
+                      onPressed: () {
+                        controller
+                            .toggleListening(); // Chuyển đổi trạng thái của micro
+                      },
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xffB2EBF2),
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  onChanged: (value) {
+                    debugPrint('Từ khóa tìm kiếm: $value');
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  HomeInWell(
+                        image: ImageAsset.combo,
+                        text: 'Combo',
+                      ),
+                  HomeInWell(
+                        image: ImageAsset.chicken,
+                        text: 'Chicken',
+                      ),
+                  HomeInWell(
+                        image: ImageAsset.burger,
+                        text: 'Burger',
+                      ),
+                  HomeInWell(
+                        image: ImageAsset.snack,
+                        text: 'Snacks',
+                      ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 211,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), // Chỉnh góc bo tròn
+                  border: Border.all(
+                      color: Colors.white,
+                      width: 2), // Đường viền của container
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      20), // Đảm bảo ảnh cũng có góc bo tròn như container
+                  child: Image.asset(
+                    ImageAsset.homeChicken,
+                    fit: BoxFit.cover, // Làm ảnh fill toàn bộ container
+                  ),
+                ),
+              ),
+              const TitleSeeMore(title: 'Combo'),
+              SizedBox(
+                height: 285, // Chiều cao cố định cho danh sách ngang
+                child: _isLoadingProducts
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Combo 1 Người' ||
+                              product['Category'] == 'Combo Nhóm') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
+              ),
+              const TitleSeeMore(title: 'Chicken'),
+          
+              SizedBox(
+                height: 285, // Chiều cao cố định cho danh sách ngang
+                child: _isLoadingProducts
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Gà Rán - Gà Quay') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
+              ),
+            
+              const TitleSeeMore(title: 'Burger - Rice - Spaghetti'),
+              SizedBox(
+                height: 285, // Chiều cao cố định cho danh sách ngang
+                child: _isLoadingProducts
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Burger - Cơm - Mì Ý') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
+              ),
+              const TitleSeeMore(title: 'Snacks'),
+              SizedBox(
+                height: 285, // Chiều cao cố định cho danh sách ngang
+                child: _isLoadingProducts
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Thức ăn nhẹ') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
