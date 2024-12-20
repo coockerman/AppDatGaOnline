07cca20a2fbba9425b0d87b17c1dd7bd853971f0
Huang <154526787+GnaoHz@users.noreply.github.com>
2024-12-21 00:32:37 +0700

    init pVM

--- lib/view_model/pofile_view_model.dart
+++ lib/view_model/pofile_view_model.dart
@@ -1,3 +1,18 @@
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import '../model/firebase/firebase_authencation.dart';
+class ProfileViewModel extends GetxController{
+  late TextEditingController searchController = TextEditingController();
+  final DatabaseReference _database = FirebaseDatabase.instance.ref();
+  final _userData = {}.obs;
+  String _userId = '';
+  RxMap get userData => _userData;
 
-class ProfileViewModel extends GetxController{}
+  @override
+  void onInit() {
+    super.onInit();
+    _initializeUserId();
+  }
+}
